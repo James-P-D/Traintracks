c6b9922db53409614598f75f5e79976002b04ee9
James Dorrington <jdorrington@hotmail.com>
2020-07-12 16:10:06 +0100

    Initial work on solver algorithm. Lots more to do..

--- src/Traintracks/Traintracks/Library.jl
+++ src/Traintracks/Traintracks/Library.jl
@@ -74,39 +74,92 @@ function is_complete(cell_cols, cell_rows, top_numbers, right_numbers, start_ter
 
     (start_terminal_col, start_terminal_row) = start_terminal
     (end_terminal_col, end_terminal_row) = end_terminal
-    col = start_terminal_col + 1
-    row = start_terminal_row + 1
+    current_col = start_terminal_col + 1
+    current_row = start_terminal_row + 1
+    end_col = end_terminal_col + 1
+    end_row = end_terminal_row + 1
     
-    while ((col != end_terminal_col + 1) && (row != end_terminal_row + 1))
-        visited_grid[col, row] = true
+    while ((current_col != end_col) && (current_row != end_row))
+        visited_grid[current_col, current_row] = true
         
-        next_cells = get_next_cell(cell_cols, cell_rows, col, row, visited_grid, grid)
+        next_cells = get_next_cell(cell_cols, cell_rows, current_col, current_row, visited_grid, grid)
 
         if (length(next_cells) != 1)
             return false
         end
         
-        (col, row) = next_cells[1]
+        (current_col, current_row) = next_cells[1]
     end
 
 	return true
 end
 
-function solve(cell_cols, cell_rows, top_numbers, right_numbers, start_terminal, end_terminal, grid)
-    println("cell_cols = ", cell_cols)
-    println("cell_rows = ", cell_rows)
-	
-    println("top_numbers = ", top_numbers)
-    println("right_numbers = ", right_numbers)
-	
-	println("start_terminal = ", start_terminal)
-	println("end_terminal = ", end_terminal)
-	
-	println("grid = ", grid)
-	
-	#for col = 1:cell_cols, row = 1:cell_rows
-    #    println(some_array[col, row])
-    #end
+function get_possible_states(current_col, current_row, next_col, next_row, grid)
+    println("get_possible_states")
+    println("current_col = ", current_col)
+    println("current_row = ", current_row)
+    println("next_col = ", next_col)
+    println("next_row = ", next_row)
+        
+    current_cell_state = grid[current_col, current_row]
+    if (current_col == next_col)
+        if (next_row < current_row)
+            return [CELL_VERTICAL, CELL_BOTTOM_LEFT, CELL_BOTTOM_RIGHT]
+        elseif (next_row > current_row)
+            return [CELL_VERTICAL, CELL_TOP_LEFT, CELL_TOP_RIGHT]
+        else
+            throw(DomainError(0, "something went wrong 1!"))
+        end
+    elseif (current_row == next_row)
+        if (next_col < current_col)
+            return [CELL_HORIZONTAL, CELL_TOP_RIGHT, CELL_BOTTOM_RIGHT]
+        elseif (next_col > current_col)
+            return [CELL_HORIZONTAL, CELL_BOTTOM_LEFT, CELL_TOP_LEFT]
+        else
+            throw(DomainError(0, "something went wrong 2!"))
+        end
+    else
+        throw(DomainError(0, "something went wrong 3!"))
+    end
+end
 
-	return is_complete(cell_cols, cell_rows, top_numbers, right_numbers, start_terminal, end_terminal, grid)
+function sub_solve(cell_cols, cell_rows, top_numbers, right_numbers, current_col, current_row, end_col, end_row, visited_grid, grid, moves)
+    println("sub_solve")
+    if ((current_col == end_col) && (current_row == end_row))
+        return (true, moves)
+    else
+        visited_grid[current_col, current_row] = true   
+        next_cells = get_next_cell(cell_cols, cell_rows, current_col, current_row, visited_grid, grid)
+
+        if (length(next_cells) != 1)
+            return (false, [])
+        else
+            println("current_col = ", current_col)
+            println("current_row = ", current_row)
+            (next_col, next_row) = next_cells[1]
+            println("next_col = ", next_col)
+            println("next_row = ", next_row)
+            
+            if (grid[next_col, next_row] != CELL_EMPTY)
+                return sub_solve(cell_cols, cell_rows, top_numbers, right_numbers, next_col, next_row, end_col, end_row, visited_grid, grid, moves)
+            else
+                possible_states = get_possible_states(current_col, current_row, next_col, next_row, grid)
+                for possible_state in possible_states
+                    println("possible_state = ", possible_state)
+                end
+                return (false, moves)
+            end            
+        end
+    end
+end
+
+function solve(cell_cols, cell_rows, top_numbers, right_numbers, start_terminal, end_terminal, grid)
+    println("solve")
+    (start_col, start_row) = start_terminal
+    (end_col, end_row) = end_terminal
+    
+    # Create an array cell_cols-by-cell_rows in size, initialised to false
+    visited_grid = fill(false, cell_cols, cell_rows)
+         
+    return sub_solve(cell_cols, cell_rows, top_numbers, right_numbers, start_col + 1, start_row + 1, end_col + 1, end_row + 1, visited_grid, grid, [])
 end
